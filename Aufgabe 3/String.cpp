10ed573105c6ac0b7dd28945b8aa856a9678df20
Niklas Schwab <49619179+DeafHorse@users.noreply.github.com>
2019-06-04 00:16:43 +0200

    My Solution
    I made some comments for the last function

--- Aufgabe 3/String.cpp
+++ Aufgabe 3/String.cpp
@@ -1,101 +1,129 @@
+// String.cpp
 #include <iostream>
 using namespace std;
 
 #include "String.h"
 
+
 String::String() {
-  size = 0;
-  str = new char[1];
-  str[0] = '\0';
+	size = 0;
+	str = new char[1];
+	str[0] = '\0';
 }
 
+
 String::String(char c) {
-  size = 1;
-  str = new char[2];
-  str[0] = c;
-  str[1] = '\0';
+	size = 1;
+	str = new char[2];
+	str[0] = c;
+	str[1] = '\0';
 }
 
 String::String(const char* s) {
-  //TODO
-  int i = 0;
-  size = 0;
-  while (s[i++]) {
-    size++;
-  }
-  str = new char[size];
-  str[size] = '\0';
-  for (i=0; i < size; i++){
-    str[i] = s[i];
-  }
+	size = 0;
+	while (*s != '\0') {
+		s++;
+		size++;
+	}
+	s -= size;
+	str = new char[(size + 1)];
+	for (int i = 0; i < size; i++) {
+		str[i] = s[i];
+	}
+	str[size] = '\0';
 }
 
-String::String(const String& s) : size(s.size), str(s.str)  {
-  //TODO
-  size = s.size;
-  str =  new char[size];
-  str[size] = '\0';
-  for (int i=0; i < size; i++){
-    str[i] = s.str[i];
-  }
+//copy Konstruktor
+String::String(const String& s) {
+	size = s.size;
+	str = new char[(size + 1)];
+	for (int i = 0; i < size; i++) {
+		str[i] = s.str[i];
+	}
+	str[size] = '\0';
 }
 
-String::String(String&& s) : size(s.size), str(s.str) {
-  //TODO
-  s.size = 0;
-  s.str = nullptr;
+//move Konstruktor
+String::String(String&& s) {
+	size = s.size;
+	str = s.str;
+	s.str = nullptr;
 }
 
-String::~String() { delete[] str;}
-
-char& String::operator[] (int index) {
-  //TODO
-  if (str[0] == '\0'){
-    return str[0];
-  }
-  return str[index];
+//Destruktor
+String::~String() {
+	delete[] str;
 }
 
+//Indexzugriff (Überladung)
+char& String::operator[](int index) {
+	return str[index];
+}
+
+//copy Zuweisung
 String& String::operator=(const String& s) {
-  //TODO
-  if (&s == this) {
-    return *this; }
-  delete[] str;
-  size = s.size;
-  str =  new char[size];
-  str[size] = '\0';
-  for (int i=0; i < size; i++){
-    str[i] = s.str[i];
-  }
-  return *this;
+	if (this != &s) {
+		size = s.size;
+		delete[] str;
+		str = new char[(size + 1)];
+		for (int i = 0; i < size; i++) {
+			str[i] = s.str[i];
+		}
+		str[size] = '\0';
+	}
+	return *this;
 }
 
+//move Zuweisung
 String& String::operator=(String&& s) {
-  //TODO
-  if (&s == this) {
-    return *this;
-  }
-  delete[] str;
-  size = s.size;
-  str = s.str;
-  s.size = 0;
-  s.str = nullptr;
-  return *this;
+	if (this != &s) {
+		delete[] str;
+		str = s.str;
+		s.str = nullptr;
+		size = s.size;
+	}
+	return *this;
 }
 
+//Konkatenation von Strings (Überladung)
 String& String::operator+=(String& s) {
-  //TODO
-  char* temp = new char[size + s.size + 1];
-  int i = 0;
-  for (i = 0; i < size; i++) {
-    temp[i] = str[i];
-  }
-  for (int j = 0, i = size; i <= size + s.size; i++, j++) {
-    temp[i] = s.str[j];
-  }
-  temp[size + s.size + 1] = '\0';
-  delete[] str;
-  str = temp;
-  size = size + s.size;
-  return *this;
+  
+	size += s.size;
+  
+  //temp char array, welches den ganzen String beinhalten soll
+	char* temp = new char[(size + 1)];
+  
+  //Kopiere den vorderen String nach temp
+	while (*str != '\0') {
+		*temp = *str;
+		temp++;
+		str++;
+	}
+  //lösche das alte this.str
+	delete[] str;
+  
+  //Kopiere den hinteren String nach temp
+	while (*s.str != '\0') {
+		*temp = *s.str;
+		temp++;
+		s.str++;
+	}
+  
+  //Wichtig! Setzt den Pointer s.str wieder auf die eigentliche Stelle
+	s.str -= s.size;
+	
+  *temp = '\0';
+	temp -= size;
+  
+  //Neues this.str wird deklariert und die einzelnen chars von temp werden nach this.str kopiert
+  //Wichtig! this.str MUSS neu deklariert werden, da es in Zeile 103 gelöscht wurde. Denn: Es wird am Ende der main nochmal gelöscht.
+  //Wenn es nicht neu deklariert wurde wird es 2x gelöscht -> runtime-exception!
+	str = new char[(size + 1)];
+	for (int i = 0; i < size; i++) {
+		str[i] = temp[i];
+	}
+	str[size] = '\0';
+  //Nur wenn die einzelnen chars kopiert werden kann temp gelöscht werden. temp muss jedoch gelöscht werden, sonst gibt es ein Speicherleck
+	delete[] temp;
+	return *this;
 }
